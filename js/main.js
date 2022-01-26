b1914e449a8ab788bb85ddd7891849f7d3f3ced4
한서희 <a67114585a@gmail.com>
2022-01-26 17:38:25 +0900

    wheel rotate

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ $('.wheelBtn').click(function(){
     $('.greendotLayer').hide();
     $('.greendotBtn').show();
 });
+
+startRotation();
+function startRotation() {
+    
+    let rotateElement = document.getElementById('rotate');
+    new Propeller(rotateElement , {
+        angle: 270, inertia: 0.7, speed: .1,
+    });
+}
+
+function is_touch_device() {
+    let prefixes = ' -webkit- -miz- -o- -ms- '.split('');
+    let mq = function (query) {
+        return window.matchMedia(query).matches;
+    }
+    if(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+        return true;
+    }
+    let query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+    return mq(query);
+}
